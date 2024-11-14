cde07f14f3b26db528b60901f3e40aa7dffb37b5
keriotisna <nk31@illinois.edu>
2024-11-14 15:35:31 -0600

    Add basic CRUD for OwnsEV

--- app/backend/evproject-backend/src/customQueries.py
+++ app/backend/evproject-backend/src/customQueries.py
@@ -1,15 +1,65 @@
-from mysql.connector.cursor import MySQLCursor
-from typing import List
-
 
 
 class Queries():
-    
-    '''
-    A generic class to hold all our queries
-    '''
-    
-    Q1 = '''
+	
+	'''
+	A generic class to hold all our queries
+	'''
+	
+	READ_UNCOMMITTED = 'SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;'
+	READ_COMMITTED = 'SET TRANSACTION ISOLATION LEVEL READ COMMITTED;'
+	REPEATABLE_READ = 'SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;'
+	SERIALIZABLE = 'SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;'
+
+	GET_OWNED_VEHICLES = '''
+SELECT
+	EV.ev_id,
+	EV.make,
+	EV.model,
+	PlugType.type_id,
+	PlugType.type_name,
+	EV.range_km,
+	EV.battery_capacity
+FROM
+	User
+	JOIN OwnsEV ON
+	User.user_id = OwnsEV.user_id
+	JOIN ElectricVehicle EV ON
+	OwnsEV.ev_id = EV.ev_id
+	JOIN PlugType ON
+	EV.plug_type = PlugType.type_id
+WHERE
+	User.user_id = %(user_id)s;
+'''
+
+	UPDATE_OWNED_VEHICLE = '''
+UPDATE   
+  OwnsEV
+SET OwnsEV.ev_id = %(new_ev_id)s
+WHERE 
+  OwnsEV.user_id = %(user_id)s
+  AND OwnsEV.ev_id = %(previous_ev_id)s;
+'''
+
+
+	INSERT_OWNED_VEHICLE = '''
+INSERT INTO 
+	OwnsEV(user_id, ev_id)
+VALUES 
+	(%(user_id)s, %(ev_id)s);
+'''
+
+
+	DELETE_OWNED_VEHICLE = '''
+DELETE FROM
+	OwnsEV
+WHERE
+	user_id = %(user_id)s
+ 	AND ev_id = %(ev_id)s;
+'''
+
+
+	Q1 = '''
 SELECT
 	Distances.ev_station_id,
 	Distances.distance_km,
@@ -32,36 +82,36 @@ SELECT
 	,2) AS price_to_charge
 FROM (
 	SELECT
-    	latitude,
-    	longitude,
-    	station_id AS ev_station_id,
-    	-- Haversine formula implementation
-    	-- R = Earth's radius in kilometers
-    	ROUND(
-        	(
-        	6371 * 2 * ASIN(
-            	SQRT(
-                	POWER(SIN(RADIANS(latitude - (%(latitude)s)) / 2), 2) +
-                	COS(RADIANS(%(latitude)s)) *
-                	COS(RADIANS(latitude)) *
-                	POWER(SIN(RADIANS(longitude - (%(longitude)s)) / 2), 2)
-            	)
-        	)
-    	), 3) AS distance_km
+		latitude,
+		longitude,
+		station_id AS ev_station_id,
+		-- Haversine formula implementation
+		-- R = Earth's radius in kilometers
+		ROUND(
+			(
+			6371 * 2 * ASIN(
+				SQRT(
+					POWER(SIN(RADIANS(latitude - (%(latitude)s)) / 2), 2) +
+					COS(RADIANS(%(latitude)s)) *
+					COS(RADIANS(latitude)) *
+					POWER(SIN(RADIANS(longitude - (%(longitude)s)) / 2), 2)
+				)
+			)
+		), 3) AS distance_km
 	FROM EVStation
 	WHERE
-    	-- Pre-filtering using bounding box
-    	-- 1 degree of latitude approx 111 km at the equator
-    	-- Variance between latitude is mostly constant, so we can just
-    	-- threshold based on the difference.
-    	-- 1 degree of longitude varies with latitude
-    	latitude
-    	BETWEEN %(latitude)s - (%(distance_threshold)s/111.0)
-        	AND %(latitude)s + (%(distance_threshold)s/111.0)
-    	AND
-    	longitude
-    	BETWEEN %(longitude)s - (%(distance_threshold)s/(111.0 * COS(RADIANS(%(latitude)s))))
-        	AND %(longitude)s + (%(distance_threshold)s/(111.0 * COS(RADIANS(%(latitude)s))))
+		-- Pre-filtering using bounding box
+		-- 1 degree of latitude approx 111 km at the equator
+		-- Variance between latitude is mostly constant, so we can just
+		-- threshold based on the difference.
+		-- 1 degree of longitude varies with latitude
+		latitude
+		BETWEEN %(latitude)s - (%(distance_threshold)s/111.0)
+			AND %(latitude)s + (%(distance_threshold)s/111.0)
+		AND
+		longitude
+		BETWEEN %(longitude)s - (%(distance_threshold)s/(111.0 * COS(RADIANS(%(latitude)s))))
+			AND %(longitude)s + (%(distance_threshold)s/(111.0 * COS(RADIANS(%(latitude)s))))
 ) AS Distances
 JOIN
 EVStation
@@ -78,4 +128,4 @@ ORDER BY
 '''
 
 
-    
+	
--- app/backend/evproject-backend/src/main.py
+++ app/backend/evproject-backend/src/main.py
@@ -1,13 +1,12 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Union
 import socket
 import uvicorn
-# Include the . before the packages since we are looking for relative imports for some reason
 from src.db_connection import DatabaseConnectionPool, getDBCursor
 from src.customQueries import Queries
 from src.smokeTest import router as smoke_test_router
-
+from mysql.connector import errorcode, Error
 import os
 
 # Uncomment this to debug locally
@@ -17,17 +16,6 @@ import os
 #     debugpy.listen(("0.0.0.0", 5678))
 #     debugpy.wait_for_client()
 
-
-
-def check_port(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        sock.bind(('0.0.0.0', port))
-        available = True
-    except:
-        available = False
-    sock.close()
-    return available
 
 app = FastAPI()
 
@@ -58,15 +46,31 @@ def health():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+# Example local connection URL for this endpoint
 # localhost:8080/CompatibleStations/?latutude=34.040539&longitude=-118.271387&distance_threshold=40&ev_id=34
+# Example deployed connection URL
 # http://localhost:8080/CompatibleStations/?latitude=34.040539&longitude=-118.271387&distance_threshold=40&ev_id=34
 @app.get("/CompatibleStations/")
-async def get_compatible_stations(
+async def getCompatibleStations(
     latitude: float = Query(..., description='Latitude of first point', ge=-90, le=90),
     longitude: float = Query(..., description='Longitude of first point', ge=-180, le=180),
     distance_threshold: float = Query(..., description='Range to search for vehicles'),
     ev_id: int = Query(..., description='ev_id of the vehicle'),
 ):
+    '''
+    An endpoint which gets all compatible EV Stations with some given ev_id, a
+    location with lat/long and a distance threshold to search over.
+    
+    Arguments:
+        latitude: Current latitude
+        longitude: Current longitude
+        distance_threshold: How many km to return results from
+        ev_id: EV ID to filter for compatible plugs
+    
+    Returns:
+        A list of records fetched meeting the constraints. 
+        Returned attributes are defined from the Q1 returns.
+    '''
     
     with getDBCursor() as cursor:
         params = {
@@ -75,25 +79,175 @@ async def get_compatible_stations(
             'distance_threshold': distance_threshold,
             'ev_id': ev_id
         }
-        
+        # We don't really care too much about reading messy data here
+        cursor.execute(Queries.READ_UNCOMMITTED)
         cursor.execute(Queries.Q1, params)
-    
         results = []
-        
         for row in cursor:
             results.append(row)
-    
     return results
 
+
+@app.get('/OwnedVehicles/')
+async def getOwnedVehicles(
+    user_id: int = Query(..., description='ID of the user')
+):
+    '''
+    An endpoint which gets all vehicles owned by a user
+    
+    Arguments:
+        user_id: The user id
+    '''
+    
+    with getDBCursor() as cursor:
+        params = {
+            'user_id': user_id
+        }
+        
+        cursor.execute(Queries.GET_OWNED_VEHICLES, params)
+        return cursor.fetchall()
+
+
+@app.put('/UpdateOwnedVehicles/')
+async def updateOwnedVehicle(
+    user_id: int = Query(..., description='ID of the user'),
+    previous_ev_id: int = Query(..., description='The ev_id of the vehicle to update'),
+    new_ev_id: int = Query(..., description='New value for the owned ev_id')
+):
+    '''
+    Allows you to update a user's owned vehicle.
+    
+    Arguments:
+        user_id: User's ID
+        previous_ev_id: ev_id to update
+        new_ev_id: New value of ev_id
+    
+    Returns:
+        All of the user's owned EVs which reflects the updated change
+    '''
+    
+    with getDBCursor() as cursor:
+        cursor.execute(Queries.SERIALIZABLE)
+        params = {
+            'user_id': user_id,
+            'previous_ev_id': previous_ev_id,
+            'new_ev_id': new_ev_id
+        }
+        
+        try:
+            # Perform update and return new results
+            cursor.execute(Queries.UPDATE_OWNED_VEHICLE, params)
+        
+        # Need to handle issues like if someone tries to enter a non-existent ev_id
+        except Error as e:
+            if e.errno == errorcode.ER_NO_REFERENCED_ROW_2:
+                raise HTTPException(
+                    status_code = status.HTTP_400_BAD_REQUEST,
+                    detail = {
+                        "message": "Invalid ev_id provided",
+                        "error_code": "ER_NO_REFERENCED_ROW_2 (Foreign key constraint)",
+                        "details": {
+                            "constraint": "ev_id",
+                            "invalid_id": new_ev_id
+                        }
+                    }
+                )
+            if e.errno == errorcode.ER_DUP_ENTRY:
+                raise HTTPException(
+                    status_code = status.HTTP_400_BAD_REQUEST,
+                    detail = {
+                        "message": "Duplicate entry: ev_id already exists",
+                        "error_code": "ER_DUP_ENTRY (Primary key constraint)",
+                        "details": {
+                            "constraint": "ev_id",
+                            "invalid_id": new_ev_id
+                        }
+                    }
+                )
+
+        return await getOwnedVehicles(user_id=user_id)
+
+
+@app.post('/InsertOwnedVehicle/')
+async def insertOwnedVehicle(
+    user_id: int = Query(..., description='ID of the user'),
+    ev_id:  int = Query(..., description='ID of the new vehicle to add')
+):
+    '''
+    Allows you to add a new EV to a user's owned vehicle types.
+    
+    Arguments:
+        user_id: User's ID
+        ev_id: ID of EV to add
+    
+    Returns:
+        All of the user's owned EVs which reflects the updated change
+    '''
+    
+    with getDBCursor() as cursor:
+        # We should be able to insert this whenever, nothing else is probably going to be affected
+        cursor.execute(Queries.READ_UNCOMMITTED)
+    
+        params = {
+            'user_id': user_id,
+            'ev_id': ev_id
+        }
+        
+        try:
+            cursor.execute(Queries.INSERT_OWNED_VEHICLE, params)
+            
+        # Need to handle issues like if someone tries to enter a non-existent ev_id
+        except Error as e:
+            if e.errno == errorcode.ER_NO_REFERENCED_ROW_2:
+                raise HTTPException(
+                    status_code = status.HTTP_400_BAD_REQUEST,
+                    detail = {
+                        "message": "Invalid ev_id provided",
+                        "error_code": "ER_NO_REFERENCED_ROW_2 (Foreign key constraint)",
+                        "details": {
+                            "constraint": "ev_id",
+                            "invalid_id": ev_id
+                        }
+                    }
+                )
+            if e.errno == errorcode.ER_DUP_ENTRY:
+                raise HTTPException(
+                    status_code = status.HTTP_400_BAD_REQUEST,
+                    detail = {
+                        "message": "Duplicate entry: user already owns this EV type",
+                        "error_code": "ER_DUP_ENTRY (Primary key constraint)",
+                        "details": {
+                            "constraint": "ev_id",
+                            "invalid_id": ev_id
+                        }
+                    }
+                )
+    
+    return await getOwnedVehicles(user_id)
+
+
+@app.delete('/DeleteOwnedVehicle/')
+async def deleteOwnedVehicle(
+    user_id: int = Query(..., description='ID of the user'),
+    ev_id: int = Query(..., description='ID of the EV to delete'),
+):
+    with getDBCursor() as cursor:
+    
+        params = {
+            'user_id': user_id,
+            'ev_id': ev_id
+        }
+        
+        cursor.execute(Queries.SERIALIZABLE)
+        cursor.execute(Queries.DELETE_OWNED_VEHICLE, params)
+    
+    return await getOwnedVehicles(user_id)
 
 # Run me with:
 # uvicorn src.main:app --host 0.0.0.0 --port 8080 --reload
 if __name__ == "__main__":
     DatabaseConnectionPool._initPool()
     port = 8080
-    if check_port(port):
-        uvicorn.run("src.main:app", host="0.0.0.0", port=port, reload=os.getenv("ENV") != "production")
-    else:
-        print(f"Port {port} is already in use. Please free the port and try again.")
+    uvicorn.run("src.main:app", host="0.0.0.0", port=port, reload=os.getenv("ENV") != "production")
     
     print('Application started!')
